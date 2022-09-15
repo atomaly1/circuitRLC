4044176818852508e19a0d795bb3e2c3e913b93b
Lucie Wabartha <lucie.wab@gmail.com>
2022-09-15 09:08:27 +0200

    classe noeud terminé (TP1)

--- noeud.py
+++ noeud.py
@@ -1,6 +1,5 @@
 from cmath import sqrt
 
-
 class Noeud:
     def __init__(self, nom: str, nx: float, ny: float):
         self._nom = nom
@@ -35,20 +34,22 @@ class Noeud:
         self._ny = ny
 
     # TODO : définir méthode __str__
+    def __str__(self):
+        return f"Noeud {self.nom} en {self.nx},{self.ny}"
 
     # TODO : définir méthode DISTANCE qui calcule la distance entre un noeud est un point de coordonnées px,py
-        def distance(self, px: float, py: float) -> float:
-            dx = self.nx - px
-            dy = self.ny - py
-            return sqrt(pow(dx,2) + pow(dy,2))
+    def distance(self, px: float, py: float) -> float:
+        dx = self.nx - px
+        dy = self.ny - py
+        return sqrt(pow(dx,2) + pow(dy,2))
 
     # TODO : définir méthode DEMANDE qui permet à l’utilisateur de créer un nœud en précisant son nom, son abscisse et son ordonnée
-        def demande() -> 'Noeud':
-            print("**CREATION D'UN NOUVEAU NOEUD**\n")
-            print("Entrer le nom du Noeud :")
-            nom = input()
-            print("Entrer la coordonnée en abscisse (x) du Noeud :")
-            nx = input()
-            print("Entrer la coordonnée en ordonnée (y) du Noeud :")
-            ny = input()
-            return Noeud(nom,nx,ny)
+    def demande() -> 'Noeud':
+        print("**CREATION D'UN NOUVEAU NOEUD**\n")
+        print("Entrer le nom du Noeud :")
+        nom = input()
+        print("Entrer la coordonnée en abscisse (x) du Noeud :")
+        nx = input()
+        print("Entrer la coordonnée en ordonnée (y) du Noeud :")
+        ny = input()
+        return Noeud(nom,nx,ny)
