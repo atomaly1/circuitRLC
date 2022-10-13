3a060e762bec18b8653c2ebf42a7db677b1da5df
LucieWab <45763701+LucieWab@users.noreply.github.com>
2022-10-14 00:39:04 +0200

    class Circuit : code + test du menu()

--- circuit.py
+++ circuit.py
@@ -1,3 +1,4 @@
+import sys
 from noeud import Noeud
 
 # TODO définir la classe CIRCUIT => attribut : liste de Noeuds 
@@ -16,10 +17,15 @@ class Circuit:
             str += f"[{noeud}]\n"
         return str 
 
+    # Retourne VRAI si le circuit n'est PAS vide, sinon retourne FAUX
+    def _circuit_non_vide(self) -> bool :
+        if len(self.noeuds) == 0 : return False
+        else : return True
+
     # Renvoie l’abscisse maximale du circuit (abscisse du noeud du circuit ayant la plus grande abscisse). Retourne 0.0 si le circuit est vide (ne contient pas de noeuds)
     def max_x(self) -> float :
         max_x = 0.0
-        if len(self.noeuds) != 0:
+        if self._circuit_non_vide :
             max_x = self.noeuds[0].nx
             for noeud in self.noeuds:
                 if noeud.nx > max_x:
@@ -41,22 +47,13 @@ class Circuit:
 
 # TODO définir méthode min_y : Analogue à max_x
 
-
-    # Renvoie le noeud le plus proche du point de coordonnées px,py. Signale une erreur si le circuit ne contient pas de noeud par : ▪ raise Exception("Circuit vide")
-    def noeud_le_plus_proche(self, px : float, py : float) -> Noeud :
-        if len(self.noeuds) != 0 :
-            noeud_proche = self.noeuds[0]
-            for noeud in self.noeuds :
-                if noeud.distance(px,py) < noeud_proche.distance(px,py) : 
-                    noeud_proche = noeud
-            return noeud_proche     
-        else : raise Exception("Circuit vide")
-
     # Retourne vrai si un noeud est contenu dans le circuit, faux sinon
+    # Soit le nom est déjà pris, soit les coordonnées sont déjà prises
     def contien_noeud(self, noeud : Noeud) -> bool:
-        if noeud in self.noeuds :
-            return True
-        else: return False
+        for elmt in self.noeuds :
+            if elmt.nom == noeud.nom : return True
+            if elmt.nx == noeud.nx and elmt.ny == noeud.ny : return True
+        return False
 
     # Ajoute un nœud au circuit. Si le nœud appartient déjà au circuit : ▪ raise Exception("le noeud est déjà dans le circuit")
     def add_noeud(self, noeud : Noeud) -> None:
@@ -70,20 +67,20 @@ class Circuit:
             raise Exception("le noeud n’est pas dans le circuit")
         else : self.noeuds.remove(noeud)
 
-# TODO méthode utilitaire pour la gestion du menu
-# TODO gérer exception si Noeud non trouvé
-# TODO à tester
-    def choisi_noeud(self) -> Noeud:
-        print(self)
-        print("Taper le nom du Noeud à sélectionner :")
-        nom_noeud_choisi = input()
+
+    # méthode utilitaire pour la gestion du menu 
+    def _choisi_noeud(self) -> int:
+        print("Taper le numéro du Noeud à sélectionner :")
         for noeud in self.noeuds :
-            if noeud.nom == nom_noeud_choisi : return noeud
-        
-'''En cours...
-# TODO définir méthode menu : permet la gestion du circuit par menu textuel : voir ci-dessous un exemple d’interaction :
-#   ▪ pour la suppression d’un nœud, vous aurez sans doute besoin de la méthode utilitaire choisiNoeud qui permet à l’utilisateur de choisir un nœud existant dans le circuit
-    def menu(self) -> bool:
+            print(self.noeuds.index(noeud)+1, ": [", noeud, "]")
+        print("0 : pour annuler la sélection")
+        num_noeud_choisi = int(input())
+        if num_noeud_choisi < 0 or num_noeud_choisi > len(self.noeuds):
+            raise Exception("choix non valide")
+        return num_noeud_choisi     
+
+    # Permet la gestion du circuit par menu textuel 
+    def menu(self) -> None:
 
         print("1 : afficher le circuit")
         print("2 : ajouter un noeud")
@@ -92,47 +89,51 @@ class Circuit:
         print("0 : quitter")
         print("votre choix ?")
 
-        input = input()
+        menu = input()
 
-        match input:
-            case '1': # afficher le circuit
-                print("Circuit{")
-                print("---------- noeuds :")
-                # TODO ajouter fonction affichage des noeuds dans 'circuit.py'
+        match menu:
+            case '1': # Afficher le circuit
+                print("Circuit{\n---------- noeuds :")
                 print(self)
                 print("}") 
-                return True
 
-            case '2': # ajouter un noeud
-                newNoeud = Noeud.demande()
-                print(newNoeud)
-                # TODO ajouter le noeud au circuit
-                self.addNoeud(newNoeud)
-                return True
+            case '2': # Ajouter un noeud
+                new_noeud = Noeud.demande()
+                print(new_noeud)
+                self.add_noeud(new_noeud)
 
-            case '3': # supprimer un noeud
-                # TODO supprimer un noeud à partir d'un menu de séléction des noeuds
-                removeNoeud(choisiNoeud())
-                return True
+            case '3': # Supprimer un noeud
+                if self._circuit_non_vide() :
+                    index = self._choisi_noeud()
+                    if index != 0 :
+                        self.remove_noeud(self.noeuds[index-1])
+                else : print("Circuit vide")
                 
-            case '4': # trouver noeud le plus proche
-                # TODO renvoyer le noeud pour lequel 'noeud.distance()' est le plus petit
-                return True
+            case '4': # Trouver noeud le plus proche
+                # renvoyer le noeud pour lequel 'noeud.distance()' est le plus petit
+                if self._circuit_non_vide() :
+                    px = float(input("abscisse : "))
+                    py = float(input("ordonnée : "))
+                    # cherche le noeud le plus proche du circuit
+                    noeud_temp = self.noeuds[0] 
+                    for noeud in self.noeuds:
+                        if noeud.distance(px,py) < noeud_temp.distance(px,py):
+                            noeud_temp = noeud
+                    print("Le noeud le plus proche de (", px, ",", py, ") est :", noeud_temp)
+                else : print("Circuit vide")
 
-            case '0': # quitter
-                return False
+            case '0': # Quitter
+                sys.exit(0)
 
             case _:
                 print("\nVeuillez entrer une réponse valide.\n")
-                return True'''
 
 # à utiliser pour tester les fonctions
 if __name__ == "__main__":
-    noeud1 = Noeud("n1",1.0,2.0)
-    noeud2 = Noeud("n2",3.0,4.0)
+
     circuit = Circuit([])
-    print("L'abscisse maximale est", circuit.max_x())
+    while(True) : circuit.menu()
     
-    circuit.add_noeud(noeud1)
-    circuit.add_noeud(noeud2)
-    print("L'abscisse maximale est", circuit.max_x())
+ 
+    
+
