841b50d56d91b4df6099bb56ef4e25298a98b23f
MelOSquid <eliott67000@gmail.com>
2022-10-14 15:47:27 +0200

    creation of class 'composant'

--- composant.py
+++ composant.py
@@ -1,2 +1,16 @@
-#  Définir la classe abstraite Composant : tout composant possède un nœud de départ, et un nœud d’arrivée (on définie un nœud de départ et un nœud d’arrivée (relation orientée), plutôt que
-# deux nœuds incidents pour pouvoir donner un sens à la valeur positive ou négative des courants/tensions).
+#  Définir la classe abstraite Composant : 
+#  tout composant possède un nœud de départ, 
+#  et un nœud d’arrivée (on définie un nœud 
+#  de départ et un nœud d’arrivée (relation 
+#  orientée), plutôt que deux nœuds incidents 
+#  pour pouvoir donner un sens à la valeur 
+#  positive ou négative des courants/tensions).
+
+from abc import ABC, abstractmethod
+from noeud import Noeud
+
+class Composant(ABC):
+
+    def __init__(self, noeud_depart: Noeud, noeud_arrivee: Noeud) -> None:
+        self._noeud_depart = noeud_depart
+        self._noeud_arrivee = noeud_arrivee
