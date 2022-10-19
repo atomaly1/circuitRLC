6e822391637b0e4c9304dc4f3ddc9f567e11a007
MelOSquid <eliott67000@gmail.com>
2022-10-20 00:27:58 +0200

    major changes on 'composant.py'

--- composant.py
+++ composant.py
@@ -6,11 +6,50 @@
 #  pour pouvoir donner un sens à la valeur 
 #  positive ou négative des courants/tensions).
 
-from abc import ABC, abstractmethod
+from abc import ABC, abstractmethod, abstractproperty
+from numbers import Complex
 from noeud import Noeud
 
 class Composant(ABC):
 
-    def __init__(self, noeud_depart: Noeud, noeud_arrivee: Noeud) -> None:
+    def __init__(self, noeud_depart: Noeud, noeud_arrivee: Noeud, nom: str, valeur: float) -> None:
         self._noeud_depart = noeud_depart
         self._noeud_arrivee = noeud_arrivee
+        self._nom = nom
+        self._valeur = valeur
+
+    # Pour être général, on va considérer que tout composant doit pouvoir calculer trois constantes
+    # (complexes) CU , CI ,CC , qui définisse l’équation linéaire générale :
+    # CU . U + CI .I = CC
+    # Compléter la définition de la classe Composant avec la déclaration des méthodes abstraites coeffU
+    # coeffI et coeffC. Spécialisez ces méthodes dans toutes les sous-classes.
+
+    @abstractproperty
+    def nom(self) -> str:
+        return self._nom
+
+    @nom.setter
+    @abstractmethod
+    def nom(self, nom: str) -> None:
+        self._nom = nom
+
+    @abstractproperty
+    def valeur(self) -> float:
+        return self._nom
+
+    @nom.setter
+    @abstractmethod
+    def valeur(self, valeur: float) -> None:
+        self._valeur = valeur
+
+    @abstractmethod
+    def coeffU(self) -> Complex:
+        pass
+
+    @abstractmethod
+    def coeffI(self) -> Complex:
+        pass
+
+    @abstractmethod
+    def coeffC(self) -> Complex:
+        pass
