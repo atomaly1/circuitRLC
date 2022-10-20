8cd81c0360ee70fd80ab9cebc12679c2ec6c40e5
MelOSquid <eliott67000@gmail.com>
2022-10-20 23:11:00 +0200

    Tested creation of resistance OK ; TODO coeff

--- resistance.py
+++ resistance.py
@@ -4,39 +4,51 @@ from numbers import Complex
 
 class Resistance(Composant):
     
-    def __init__(self, noeud_depart: Noeud, noeud_arrivee: Noeud, nom: str, valeur: float) -> None:
-        super().__init__(noeud_depart, noeud_arrivee, nom, valeur)
+    def __init__(self, noeud_depart: Noeud, noeud_arrivee: Noeud, nom: str, r: float) -> None:
+        super().__init__(noeud_depart, noeud_arrivee)
+        self._nom = nom
+        self._r = r
 
     @property
     def nom(self) -> str:
-        return super().nom
+        return self._nom
 
     @nom.setter
-    def nom_setter(self, nom: str) -> None:
-        return super().nom_setter
+    def nom(self, nom: str) -> None:
+        self._nom = nom
 
     @property
-    def valeur(self) -> float:
-        return super().valeur
+    def r(self) -> float:
+        return self._r
 
-    @valeur.setter
-    def valeur_setter(self, valeur: float) -> None:
-        return super().valeur_setter
+    @r.setter
+    def r(self, r: float) -> None:
+        self._r = r
 
-    def coeffU(self) -> float:
-        return complex(0)
+    def __str__(self) -> str:
+        return f"[Resistance {self.nom} ({self.noeud_depart} -> {self.noeud_arrivee}) : {self.r} Ohm]"
 
-    def coeffI(self) -> Complex:
-        return complex(self.valeur)
+# TODO
+    # def bordel(Cu, Ci, Cc) -> question pour le prof
+    #   Cu.U + Ci.I = Cc 
+    #   return C Koa se bordel ?
 
-    def coeffC(self, U: float, I: float) -> Complex:
-#TODO fonction pour calculer le module d'un complexe
-        return 0 #0 à changer avec module(self.coeffU)*U + module(self.coeffI)*I
+    def coeff_u(self) -> Complex:
+        pass
+
+    def coeff_i(self) -> Complex:
+        pass
+
+    def coeff_c(self, U: float, I: float) -> Complex:
+        pass 
 
 if __name__ == "__main__":
     n1 = Noeud("n1", 0, 100)
+    print(n1)
     n2 = Noeud("n2", 100, 100)
-    R1 = Resistance("R1", 100, n1, n2)
+    print(n2)
+    r1 = Resistance(n1, n2, "R1", 200)
+    print(r1)
     
     #TEST NON FONCTIONNEL
     #c1 = R1.coeffI
